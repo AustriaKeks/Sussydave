e494ea7d7b0caf25d86cd928f7d031d9610822e7
AustriaKeks <120101564+AustriaKeks@users.noreply.github.com>
2023-05-08 14:22:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// Create a new heading element
+const heading = document.createElement("h1");
 
-
+// Set the text content of the heading to "amongus"
+heading.textContent = "amongus";
+
+// Set the font size of the heading to 30
+heading.style.fontSize = "30px";
+
+// Set the text alignment of the heading to center
+heading.style.textAlign = "center";
+
+// Add the heading element to the body of the page
+document.body.appendChild(heading);
+
